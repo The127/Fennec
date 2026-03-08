8c6d986b399c954313a86206758e35f9353b55a5
karo <karolin.kostial@gmail.com>
2026-03-08 10:36:03 +0100

    add client implementation for server management, including `ServerClient` and `IServerClient`

---
+++ Fennec.Client/Clients/Server.cs
@@ -0,0 +1,32 @@
+using System.Net.Http.Json;
+using Fennec.Shared.Dtos.Server;
+
+namespace Fennec.Client.Clients;
+
+public interface IServerClient
+{
+    Task<CreateServerResponseDto> CreateServerAsync(CreateServerRequestDto request, CancellationToken cancellationToken = default);
+    Task JoinServerAsync(Guid serverId, CancellationToken cancellationToken = default);
+}
+
+public class ServerClient(HttpClient httpClient) : IServerClient
+{
+    public async Task<CreateServerResponseDto> CreateServerAsync(CreateServerRequestDto request, CancellationToken cancellationToken = default)
+    {
+        var uri = new Uri("api/v1/server/create");
+        
+        var response = await httpClient.PostAsJsonAsync(uri, request, cancellationToken);
+        response.EnsureSuccessStatusCode();
+        
+        var responseDto = await response.Content.ReadFromJsonAsync<CreateServerResponseDto>(cancellationToken: cancellationToken);
+        return responseDto ?? throw new Exception("Error decoding response.");
+    }
+
+    public async Task JoinServerAsync(Guid serverId, CancellationToken cancellationToken = default)
+    {
+        var uri = new Uri($"api/v1/server/{serverId}/join");
+
+        var response = await httpClient.PostAsync(uri, null, cancellationToken);
+        response.EnsureSuccessStatusCode();
+    }
+}
