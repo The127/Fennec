2fcd3dffdc4db9cf8e2f84d92114f70dee22207d
Juce <iamjuceuwu@gmail.com>
2026-03-09 21:04:25 +0000

    bound login controls

--- Fennec.App/ViewModels/LoginViewModel.cs
+++ Fennec.App/ViewModels/LoginViewModel.cs
@@ -1,9 +1,23 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Fennec.App.ViewModels;
 
 public partial class LoginViewModel : ViewModelBase
 {
     [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
     private string _username = "";
+    
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
+    private string _password = "";
+
+    private bool CanLogin() => !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password);
+
+    [RelayCommand(CanExecute = nameof(CanLogin))]
+    public void Login()
+    {
+        Console.WriteLine($"username: {Username}, password: {Password}");
+    }
 }
