bbbd2b04f9eeebc06e2ece19fd3f1ff70518402d
karo <karolin.kostial@gmail.com>
2026-03-10 14:09:39 +0100

    refactor LoginViewModel to use partial properties and parse instance URL from username during login

--- Fennec.App/ViewModels/LoginViewModel.cs
+++ Fennec.App/ViewModels/LoginViewModel.cs
@@ -8,17 +8,20 @@ public partial class LoginViewModel(IAuthService authService) : ViewModelBase
 {
     [ObservableProperty]
     [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
-    private string _username = "";
-    
+    public partial string Username { get; set; }
+
     [ObservableProperty]
     [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
-    private string _password = "";
+    public partial string Password { get; set; }
 
     private bool CanLogin() => !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password);
 
     [RelayCommand(CanExecute = nameof(CanLogin))]
     private async Task Login(CancellationToken cancellationToken)
     {
-        await authService.Login(Username, Password, "https://localhost:7014/", cancellationToken);
+        var usernameParts = Username.Split('@');
+        var username = usernameParts[0];
+        var instanceUrl = usernameParts[1];
+        await authService.Login(Username, Password, instanceUrl, cancellationToken);
     }
 }
