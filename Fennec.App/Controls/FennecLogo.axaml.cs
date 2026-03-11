7724605a358aeb27fd5d9710eef66447dd2df706
karo <karolin.kostial@gmail.com>
2026-03-11 23:25:28 +0100

    add new logo and integrate it into the app: update views and controls to display `FennecLogo`

---
+++ Fennec.App/Controls/FennecLogo.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Fennec.App.Controls;
+
+public partial class FennecLogo : UserControl
+{
+    public FennecLogo()
+    {
+        InitializeComponent();
+    }
+}
