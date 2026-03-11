3d7398bbce887429cb93bfd16ceccc7c4ead02a2
karo <karolin.kostial@gmail.com>
2026-03-11 17:01:25 +0100

    add validation attributes to RegisterViewModel, refactor constructor, and enhance registration logic

--- Fennec.App/ViewModels/RegisterViewModel.cs
+++ Fennec.App/ViewModels/RegisterViewModel.cs
@@ -1,20 +1,36 @@
+using System.ComponentModel.DataAnnotations;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Fennec.App.Messages;
 using Fennec.App.Services.Auth;
 using CommunityToolkit.Mvvm.Messaging;
+using Fennec.App.Validators;
 
 namespace Fennec.App.ViewModels;
 
-public partial class RegisterViewModel(IAuthService authService) : ObservableRecipient
+[ObservableRecipient]
+public partial class RegisterViewModel : ObservableValidator
 {
     [ObservableProperty]
+    [NotifyDataErrorInfo]
+    [Required(ErrorMessage = "Username is required")]
+    [UsernameFormat]
     [NotifyCanExecuteChangedFor(nameof(RegisterCommand))]
     private string _username = "";
     
     [ObservableProperty]
+    [NotifyDataErrorInfo]
+    [Required(ErrorMessage = "Password is required")]
     [NotifyCanExecuteChangedFor(nameof(RegisterCommand))]
     private string _password = "";
+
+    private readonly IAuthService _authService;
+
+    public RegisterViewModel(IAuthService authService, IMessenger messenger)
+    {
+        _authService = authService;
+        Messenger = messenger;
+    }
 
     private bool CanRegister() => !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password);
     
@@ -24,7 +40,7 @@ public partial class RegisterViewModel(IAuthService authService) : ObservableRec
         var usernameParts = Username.Split('@');
         var username = usernameParts[0];
         var instanceUrl = usernameParts[1];
-        await authService.RegisterAsync(username, Password, instanceUrl, cancellationToken);
+        await _authService.RegisterAsync(username, Password, instanceUrl, cancellationToken);
         Messenger.Send(new AuthNavigationMessage(AuthNavigationTarget.Login));
     }
     
